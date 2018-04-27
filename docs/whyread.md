5e8e90ac187cddbbff0c2c117210c6ca59537688
Jonathan Carson <jonathan.k.carson@gmail.com>
2018-04-27 16:49:04 -0400

    Initial setup of BT help docs

---
+++ docs/whyread.md
@@ -0,0 +1,7 @@
+---
+id: whyread
+title: Why Read?
+---
+
+This is a link to [another document.](doc3.md)  
+This is a link to an [external page.](http://www.example.com)
---
+++ website/siteConfig.js
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config.html for all the possible
+// site configuration options.
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'Zoobean',
+    image: '/test-site/img/docusaurus.svg',
+    infoLink: 'https://www.zoobean.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'Beanstack Tracker Help' /* title for your website */,
+  tagline: 'Help documentation for Beanstack Tracker mobile app',
+  // For github.io type URLs, you would set the url and baseUrl like:
+  url: 'https://zoobean.github.io',
+  baseUrl: '/beanstack-tracker-help/',
+
+  // Used for publishing and more
+  projectName: 'beanstack-tracker-help',
+  organizationName: 'zoobean',
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'intro', label: 'Help'},
+    {doc: 'whyread', label: 'Why Read?'},
+    {doc: 'about', label: 'About'}
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus.svg',
+  favicon: 'img/favicon.png',
+
+  /* colors for website */
+  colors: {
+    primaryColor: '#2E8555',
+    secondaryColor: '#205C3B',
+  },
+
+  /* custom fonts for website */
+  /*fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },*/
+
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    'Zoobean',
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  /* On page navigation for the current documentation page */
+  onPageNav: 'separate',
+
+  /* Open Graph and Twitter card images */
+  ogImage: 'img/docusaurus.png',
+  twitterImage: 'img/docusaurus.png',
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
